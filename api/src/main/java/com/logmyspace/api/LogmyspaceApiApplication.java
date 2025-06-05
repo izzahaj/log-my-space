77ed16d01c0c978faf75fa53e8b48b85736f0865
izzahaj <izzah.jalil02@gmail.com>
2025-06-05 23:20:14 +0800

    chore: test pre-commit hook api

--- api/src/main/java/com/logmyspace/api/LogmyspaceApiApplication.java
+++ api/src/main/java/com/logmyspace/api/LogmyspaceApiApplication.java
@@ -3,12 +3,10 @@ package com.logmyspace.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The main entry point to the Log My Space API.
- */
+/** The main entry point to the Log My Space API. */
 @SpringBootApplication
 public class LogmyspaceApiApplication {
-  
+
   public static void main(String[] args) {
     SpringApplication.run(LogmyspaceApiApplication.class, args);
   }
